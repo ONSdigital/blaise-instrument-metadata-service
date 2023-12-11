7b935d4cdb593f68c66a74882dc7a07ca5a2b89d
MD Maruf Motaleb <md.maruf.motaleb@ons.gov.uk>
2023-12-11 18:32:03 +0000

    wip: changed import orders around

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
-from .app import app, init_datastore
-from .tm_release_date import tmreleasedate
 from .to_start_date import tostartdate
+from .tm_release_date import tmreleasedate
+from .app import app, init_datastore
 
 __all__ = ["app", "tostartdate", "tmreleasedate", "init_datastore"]
