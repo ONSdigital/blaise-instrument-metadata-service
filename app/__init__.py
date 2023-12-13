9e49895021a66fa641c03d8e8a5946581c8aa6ae
James Anthony Williams <81305008+JAWilliamsONS@users.noreply.github.com>
2023-12-13 19:19:48 +0000

    refactor: remove commented out code

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,3 @@ from .tm_release_date import tmreleasedate
 from .to_start_date import tostartdate
 
 __all__ = ["app", "tostartdate", "tmreleasedate", "init_datastore"]
-
-
-'''
-from .to_start_date import tostartdate
-from .tm_release_date import tmreleasedate
-from .app import app, init_datastore
-
-__all__ = ["app", "tostartdate", "tmreleasedate", "init_datastore"]
-'''
